88663a1d88199a334bf59c622a6490dc63479467
melcd238 <melcd238@gmail.com>
2022-06-26 21:28:48 +0200

    authGard for profil route

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import  Error  from './Pages/Error';
 import  Profil  from './Pages/Profil';
 import  SignIn  from './Pages/SignIn';
 import Layout from './Layout/Layout';
+import AuthGuard from './HOC/authGuard';
 
 
 
@@ -16,7 +17,7 @@ function Router() {
    <Routes>
      <Route path="/" element={<Home />}></Route>
      <Route path="login" element={<SignIn/>}></Route>
-     <Route path="profil" element={<Profil/>}></Route>  
+     <Route path="profil" element={<AuthGuard><Profil/></AuthGuard>}></Route>  
      <Route path="*" element={<Error />} />
    </Routes>
    </Layout>
