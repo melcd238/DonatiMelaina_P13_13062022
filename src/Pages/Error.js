c6411d1bcecfbc50b1d5edeb6f785b032a4b07bb
melcd238 <melcd238@gmail.com>
2022-06-27 08:39:46 +0200

    errorPage

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,10 +1,12 @@
-
+import { Link } from "react-router-dom";
 
 const Error =()=>{
     return(
-        <>
-        Error
-        </>
+        <main className="main bg-dark">
+         <h1>OUUUUUUUPS!!!! <br/> La page que vous demandez <br/> N'existe pas...</h1>
+         <p className="errorPage">404</p>
+         <Link to="/"> Retournez à la page d'accueil</Link>
+        </main>
     )
 }
 
