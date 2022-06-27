24ac0e2a67cf0bb3eb7a08bfc0c240bd5e90213e
melcd238 <melcd238@gmail.com>
2022-06-27 10:14:54 +0200

    add redux-persist with sessionStorage

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,11 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UsersReducer from "./Reducers/Users";
-import NotificationReducer from './Reducers/Notification'
+import NotificationReducer from './Reducers/Notification';
+import storage from "redux-persist/lib/storage/session";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+
+const persistConfig ={
+  key:'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, UsersReducer)
 
 
 export const store = configureStore({
     reducer:{
-      users: UsersReducer,
-      notification: NotificationReducer
+      users: persistedReducer,
+      notification: NotificationReducer,
+      middleware: [thunk]
     }
 })
+
+export const persistor = persistStore(store)
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './Styles/main.css'
 import Router from './Router';
 import { Provider } from 'react-redux';
-import { store } from './Store';
+import {  store, persistor } from './Store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 
 
@@ -11,9 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <Provider store={store}>
+    <PersistGate loadin={null} persistor={persistor}>
     <BrowserRouter>
         <Router />
     </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
 
